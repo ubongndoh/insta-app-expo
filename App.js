bc6ec2ef38ff0f82be21c6f99eb44bd1b97e1411
Ubong Ndoh <ubongndoh@gmail.com>
2021-05-29 23:25:17 +0100

    finished app

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, View, FlatList } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import Constants from 'expo-constants';
+import Header from './components/Header';
+import Stories from './components/Stories';
+import Article from './components/Article';
+import { articles } from './data';
 
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Hello World.</Text>
-			<StatusBar style='auto' />
+export default function Instagram() {
+	const renderItem = ({ item }) => <Article item={item} />;
+	const renderHeader = () => (
+		<View style={styles.stories}>
+			<Stories />
 		</View>
 	);
+	const keyExtractor = (item) => item.id.toString();
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<StatusBar style='dark' />
+
+			<View style={styles.header}>
+				<Header />
+			</View>
+
+			{/* https://reactnative.dev/docs/flatlist */}
+			<FlatList
+				data={articles}
+				renderItem={renderItem}
+				ListHeaderComponent={renderHeader}
+				keyExtractor={keyExtractor}
+				showsVerticalScrollIndicator={false}
+			/>
+		</SafeAreaView>
+	);
 }
+
+const BORDER_BOTTOM = {
+	borderBottomWidth: 1,
+	borderBottomColor: '#dbdbdb',
+};
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		paddingTop: Constants.statusBarHeight,
+	},
+	header: {
+		...BORDER_BOTTOM,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
 		alignItems: 'center',
-		justifyContent: 'center',
+		paddingHorizontal: 16,
+		height: 44,
+	},
+	stories: {
+		...BORDER_BOTTOM,
+		height: 104,
+		paddingVertical: 10,
+		paddingLeft: 8,
+		backgroundColor: '#fafafa',
 	},
 });
---
+++ data.js
@@ -0,0 +1,73 @@
+export const profile = {
+	avatar: require('./assets/instagram-feed/avatar6.jpg'),
+};
+
+export const stories = [
+	{
+		id: 1,
+		avatar: require('./assets/instagram-feed/avatar1.jpg'),
+		name: 'chelsea',
+		isSeen: false,
+	},
+	{
+		id: 2,
+		avatar: require('./assets/instagram-feed/avatar2.jpg'),
+		name: 'teamXbox',
+		isSeen: false,
+	},
+	{
+		id: 3,
+		avatar: require('./assets/instagram-feed/avatar3.jpg'),
+		name: 'Jamal',
+		isSeen: true,
+	},
+	{
+		id: 4,
+		avatar: require('./assets/instagram-feed/avatar4.jpg'),
+		name: 'peeps',
+		isSeen: true,
+	},
+];
+
+export const articles = [
+	{
+		id: 1,
+		avatar: require('./assets/instagram-feed/avatar1.jpg'),
+		name: 'papsi',
+		image: require('./assets/instagram-feed/img1.jpg'),
+		likeCount: '23',
+		date: '1 min ago',
+	},
+	{
+		id: 2,
+		avatar: require('./assets/instagram-feed/avatar1.jpg'),
+		name: 'lego',
+		image: require('./assets/instagram-feed/avatar6.jpg'),
+		likeCount: '46',
+		date: '3 mins ago',
+	},
+	{
+		id: 3,
+		avatar: require('./assets/instagram-feed/avatar3.jpg'),
+		name: 'hamas',
+		image: require('./assets/instagram-feed/img3.jpg'),
+		likeCount: '72',
+		date: '8 mins ago',
+	},
+	{
+		id: 4,
+		avatar: require('./assets/instagram-feed/avatar2.jpg'),
+		name: 'teamXbox',
+		image: require('./assets/instagram-feed/img4.jpg'),
+		likeCount: '19',
+		date: '1 hour ago',
+	},
+	{
+		id: 5,
+		avatar: require('./assets/instagram-feed/avatar2.jpg'),
+		name: 'teamXbox',
+		image: require('./assets/instagram-feed/img5.jpg'),
+		likeCount: '12',
+		date: '4 hours ago',
+	},
+];
